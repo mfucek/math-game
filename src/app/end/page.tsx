2b446002315b5e3a769375a908186ed333b91290
Matija Fućek <matijafucek1@gmail.com>
2023-09-24 19:20:38 +0200

    unsuccessful PWA conversion

--- src/app/end/page.tsx
+++ src/app/end/page.tsx
@@ -1,27 +1,14 @@
-'use client';
+import { EndComp } from './End';
 
-import { useScore } from '@/components/ScoreProvider';
-import Link from 'next/link';
+export const metadata = {
+	title: 'Next PWA',
+	description: 'Next PWA example using next-pwa and app directory'
+};
 
-const End = () => {
-	const { score, setScore } = useScore();
+export default () => {
 	return (
 		<>
-			<div className="absolute w-full h-full flex flex-col items-center justify-center">
-				<h1 className="text-9xl font-bold mb-10">Game Over</h1>
-				<h2 className="text-4xl mb-10">Score: {score}</h2>
-				<Link href="/">
-					<button
-						className="font-semibold text-4xl bg-blue-600 rounded-full h-20 px-8"
-						onClick={() => {
-							setScore(0);
-						}}>
-						Play Again
-					</button>
-				</Link>
-			</div>
+			<EndComp />
 		</>
 	);
 };
-
-export default End;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { Thing } from '@/components/Thing';
 
+export const metadata = {
+	title: 'Next PWA',
+	description: 'Next PWA example using next-pwa and app directory'
+};
+
 export default function Home() {
 	return (
 		<main className="">
